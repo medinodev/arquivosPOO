a6d2bdab32991c7a74acf79f2a9104c0f922c2b9
medinodev <medino.dev@gmail.com>
2024-04-07 15:18:59 -0300

    arquivos

---
+++ exercicioSimples/Animal.java
@@ -0,0 +1,16 @@
+package exercicioSimples;
+
+public class Animal {
+
+    protected String nome;
+    protected double peso;
+
+
+    public String getNome(){ return nome; }
+    public void setNome(String nome){ this.nome = nome; }
+
+    public double getPeso(){ return peso; }
+    public void setPeso(double peso){ this.peso = peso; }
+
+    
+}
