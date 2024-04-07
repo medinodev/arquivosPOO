cee6696bd6346252a1aa0c560554cf3eae4979fd
medinodev <medino.dev@gmail.com>
2024-04-07 14:50:33 -0300

    commit 1

---
+++ Animal.java
@@ -0,0 +1,13 @@
+package arquivosPOO;
+
+public class Animal {
+
+    protected String nome;
+    protected double peso;
+
+
+    public String getNome(){ return nome; }
+    public void setNome(String nome){ this.nome = nome; }
+
+    
+}
