af43983dbf6e35bbb9decfc26ce94119c8c9cd69
Mayara Medino <146399732+medinodev@users.noreply.github.com>
2024-04-08 19:30:36 -0300

    Update Gerente.java
    Comentários

--- exercicioSimple2/Gerente.java
+++ exercicioSimple2/Gerente.java
@@ -4,6 +4,10 @@ public class Gerente extends Empregado{
     //Protected não está errado, estmaos alterando só por que a questão está pedindo.
     private String nomeGerencia;
 
+    //to-do: get e set
+
+    //to-do: metodo toString com nome, idade, matrícula, nome gerência e valor do INSS.
+
 
 
 }
