1dfd927bec4ae91b15b56fdcf61ca370fd7a85a9
medinodev <medino.dev@gmail.com>
2024-04-07 23:14:05 -0300

    exercicio 3

--- exercicioSimple2/Empregado.java
+++ exercicioSimple2/Empregado.java
@@ -4,5 +4,7 @@ public class Empregado {
     protected double salario;
     protected String matricula;
 
-
+    public double valorINSS(double salario){
+        return salario * 0.11;
+    }
 }
--- exercicioSimple2/Gerente.java
+++ exercicioSimple2/Gerente.java
@@ -3,8 +3,6 @@ package exercicioSimple2;
 public class Gerente {
     protected String nomeGerencia;
 
-    public double valorINSS(double salario){
-        return salario * 0.11;
-    }
+
 
 }
