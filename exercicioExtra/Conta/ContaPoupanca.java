beb9d922e666158e3d2840de3efceb3c632e2fd0
Mayara Medino <146399732+medinodev@users.noreply.github.com>
2024-04-16 13:02:20 -0300

    Update ContaPoupanca.java

--- exercicioExtra/Conta/ContaPoupanca.java
+++ exercicioExtra/Conta/ContaPoupanca.java
@@ -2,5 +2,15 @@ package exercicioExtra.Conta;
 
 public class ContaPoupanca extends Conta{
 
-
+     @Override
+    public void realizarSaque(float valorSaque) {
+        if(valorSaque <= 500){
+            float saque = saldo - valorSaque;
+            System.out.println("O valor sacado foi " + valorSaque + "seu saldo anterior era de " + saldo + "devido o saque" 
+                               " seu saldo atual é " + saque);
+        } else {
+            System.out.println("Seu limite de saque é de ate R$ 500,00. O saque que voce solicitou" +
+                    " excede seu limite, por favor tente novamente. " + "O valor de saque solicitado anteriormente" +
+                    " foi de R$ " + valorSaque );
+        }
 }
